ca67b066b1cfe21beff567959c55acec357f7a74
Aaditya Sharma <aadityaja@gmail.com>
2016-07-05 20:24:39 +0530

    Add files via upload

--- Ledger/src/ledger/LedgeGUI.java
+++ Ledger/src/ledger/LedgeGUI.java
@@ -1,12 +1,12 @@
 
 package ledger;
 
-
 public class LedgeGUI extends javax.swing.JFrame {
 
     
     public LedgeGUI() {
         initComponents();
+        
     }
 
     /**
@@ -18,17 +18,18 @@ public class LedgeGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jToggleButton1 = new javax.swing.JToggleButton();
         data_tab = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
-        name = new javax.swing.JTextField();
-        tmark = new javax.swing.JTextField();
+        tf_name = new javax.swing.JTextField();
+        tf_tmark = new javax.swing.JTextField();
         newentry = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        item = new javax.swing.JTextField();
-        quantity = new javax.swing.JTextField();
-        price = new javax.swing.JTextField();
+        tf_item = new javax.swing.JTextField();
+        tf_quantity = new javax.swing.JTextField();
+        tf_price = new javax.swing.JTextField();
         submit = new javax.swing.JButton();
         jLabel9 = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
@@ -59,17 +60,19 @@ public class LedgeGUI extends javax.swing.JFrame {
         jMenuItem4 = new javax.swing.JMenuItem();
         contact = new javax.swing.JMenuItem();
 
+        jToggleButton1.setText("jToggleButton1");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Ledger");
-        setType(java.awt.Window.Type.UTILITY);
+        setResizable(false);
 
         data_tab.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         data_tab.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         data_tab.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
 
-        name.addActionListener(new java.awt.event.ActionListener() {
+        tf_name.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                nameActionPerformed(evt);
+                tf_nameActionPerformed(evt);
             }
         });
 
@@ -80,33 +83,38 @@ public class LedgeGUI extends javax.swing.JFrame {
             }
         });
 
-        jLabel3.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel3.setText("Item                 ");
 
-        jLabel4.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel4.setText("Quantity (Kg)   ");
 
-        jLabel5.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel5.setText("Total Price        ");
 
-        item.addActionListener(new java.awt.event.ActionListener() {
+        tf_item.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                itemActionPerformed(evt);
+                tf_itemActionPerformed(evt);
             }
         });
 
-        price.addActionListener(new java.awt.event.ActionListener() {
+        tf_price.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                priceActionPerformed(evt);
+                tf_priceActionPerformed(evt);
             }
         });
 
         submit.setText("Submit");
+        submit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                submitActionPerformed(evt);
+            }
+        });
 
-        jLabel9.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel9.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel9.setText("Trade Mark");
 
-        jLabel10.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel10.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel10.setText("Name");
 
         jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
@@ -116,7 +124,7 @@ public class LedgeGUI extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(409, Short.MAX_VALUE)
+                .addContainerGap(400, Short.MAX_VALUE)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 12, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
@@ -125,52 +133,52 @@ public class LedgeGUI extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(price, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(tf_price, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel10)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(name, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(tf_name, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel9)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(tmark, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(tf_tmark, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel4)
                             .addComponent(jLabel3))
                         .addGap(55, 55, 55)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(item, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(quantity, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(tf_item, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(tf_quantity, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addGap(101, 101, 101))
         );
 
-        jPanel1Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {name, tmark});
+        jPanel1Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {tf_name, tf_tmark});
 
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(59, 59, 59)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tf_name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel10))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(tmark, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tf_tmark, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel9))
                 .addGap(18, 18, 18)
                 .addComponent(newentry)
                 .addGap(40, 40, 40)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(item, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(tf_item, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel4)
-                    .addComponent(quantity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(tf_quantity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(price, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tf_price, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel5))
                 .addGap(18, 18, 18)
                 .addComponent(submit)
@@ -212,21 +220,35 @@ public class LedgeGUI extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(jTable1);
 
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jLabel6.setText("Name");
 
+        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jLabel7.setText("Trade Mark");
 
+        jLabel8.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jLabel8.setText("Item");
 
+        jTextField2.setEditable(false);
+
+        jLabel11.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jLabel11.setText("Quantity");
 
+        jLabel12.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jLabel12.setText("Price");
 
+        jTextField3.setEditable(false);
         jTextField3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextField3ActionPerformed(evt);
             }
         });
+
+        jTextField4.setEditable(false);
+
+        jTextField5.setEditable(false);
+
+        jTextField6.setEditable(false);
 
         jButton2.setText("Edit");
 
@@ -266,7 +288,7 @@ public class LedgeGUI extends javax.swing.JFrame {
                     .addComponent(jLabel12)
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jButton2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 82, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 83, Short.MAX_VALUE)
                         .addComponent(jButton3))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jButton4)
@@ -275,7 +297,7 @@ public class LedgeGUI extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
                 .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 511, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 510, Short.MAX_VALUE))
         );
 
         jPanel2Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {jButton2, jButton3, jButton4, jButton5});
@@ -355,6 +377,7 @@ public class LedgeGUI extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -369,25 +392,49 @@ public class LedgeGUI extends javax.swing.JFrame {
         
     }//GEN-LAST:event_contactActionPerformed
 
-    private void priceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_priceActionPerformed
+    private void tf_priceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tf_priceActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_priceActionPerformed
+    }//GEN-LAST:event_tf_priceActionPerformed
 
-    private void itemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemActionPerformed
+    private void tf_itemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tf_itemActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_itemActionPerformed
-
+    }//GEN-LAST:event_tf_itemActionPerformed
+    
+    // Clears all the data in the textfields of Entry tab.
     private void newentryActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_newentryActionPerformed
-        // TODO add your handling code here:
+        tf_name.setText("");
+        tf_tmark.setText("");
+        tf_item.setText("");
+        tf_quantity.setText("");
+        tf_price.setText("");
     }//GEN-LAST:event_newentryActionPerformed
 
-    private void nameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nameActionPerformed
+    private void tf_nameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tf_nameActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_nameActionPerformed
+    }//GEN-LAST:event_tf_nameActionPerformed
 
     private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField3ActionPerformed
+    
+    // Adds data into the database using the functions defined in the package ledger.
+    private void submitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submitActionPerformed
+        Ledger lg = new Ledger();
+        String name = tf_name.getText();
+        String tmark = tf_tmark.getText();
+        String item = tf_item.getText();
+        int quantity = Integer.parseInt(tf_quantity.getText());
+        int price = Integer.parseInt(tf_price.getText());
+        try{
+            lg.insertToDb(name, tmark.toUpperCase(), item, quantity, price);
+        }
+    
+         catch(Exception e){      
+        }
+        tf_item.setText("");
+        tf_quantity.setText("");
+        tf_price.setText("");
+    }//GEN-LAST:event_submitActionPerformed
 
     /**
      * @param args the command line arguments
@@ -427,7 +474,6 @@ public class LedgeGUI extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenuItem contact;
     private javax.swing.JTabbedPane data_tab;
-    private javax.swing.JTextField item;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
@@ -459,11 +505,13 @@ public class LedgeGUI extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField4;
     private javax.swing.JTextField jTextField5;
     private javax.swing.JTextField jTextField6;
-    private javax.swing.JTextField name;
+    private javax.swing.JToggleButton jToggleButton1;
     private javax.swing.JButton newentry;
-    private javax.swing.JTextField price;
-    private javax.swing.JTextField quantity;
     private javax.swing.JButton submit;
-    private javax.swing.JTextField tmark;
+    private javax.swing.JTextField tf_item;
+    private javax.swing.JTextField tf_name;
+    private javax.swing.JTextField tf_price;
+    private javax.swing.JTextField tf_quantity;
+    private javax.swing.JTextField tf_tmark;
     // End of variables declaration//GEN-END:variables
 }
--- Ledger/src/ledger/Ledger.java
+++ Ledger/src/ledger/Ledger.java
@@ -1,10 +1,101 @@
-
+/**
+ * THIS CLASS CONTAINS ALL THE NECESSRY FUNCTIONS TO EXECUTE SQL QUERIES.
+ */
 package ledger;
+import java.sql.*;
 
 
 public class Ledger  {
-
+   public Connection c = null;
+   public Statement stmt = null;
+   
+   private String name_db = "";
+   private String tm_db = "";
+   private String item_db = "";
+   private int quantity_db = 0;
+   private int price_db = 0;
+   
+    
+    // Connecting to the database.
+    public void connectDb(){        
+        try{
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:Ledger.db");
+            
+        } 
+        catch ( ClassNotFoundException | SQLException e ){        
+        }    
+    }
+    
+    // Query
+    public void insertToDb(String n, String t, String i, int q, int p){
+        name_db = n;
+        tm_db = t;
+        item_db = i;
+        quantity_db = q;
+        price_db = p;
+        
+        try{
+            connectDb();
+            stmt = c.createStatement();
+            String query = "INSERT INTO ledger_table (Name, Trade Mark, Item, Quantity, Price) VALUES (name_db, tm_db, item_db, quantity_db, price_db)";
+            stmt.executeUpdate(query);            
+            stmt.close();
+            c.commit();
+            c.close();           
+            
+        }
+        catch(Exception e){            
+        }             
+    }
+    
+    public void searchName(String n){
+        name_db = n;
+        try{
+            connectDb();
+            stmt = c.createStatement();
+            String query = "SELECT * FROM ledger_table WHERE Name = name_db";
+            stmt.executeUpdate(query);
+            stmt.close();
+            c.commit();
+            c.close();
+        }
+        catch(Exception e){
+            
+        }
+    }
+    
+    public void searchTM(String t){
+        tm_db = t;
+        try{
+            connectDb();
+            stmt = c.createStatement();
+            String query = "SELECT * FROM ledger_table WHERE Trade Mark = tm_db";
+            stmt.executeUpdate(query);
+            stmt.close();
+            c.commit();
+            c.close();
+        }
+        catch(Exception e){
+            
+        }
+    }
+    
+    public void searchItem(String i){
+        item_db = i;
+        try{
+            connectDb();
+            stmt = c.createStatement();
+            String query = "SELECT * FROM ledger_table WHERE Item = item_db";
+            stmt.executeUpdate(query);
+            stmt.close();
+            c.commit();
+            c.close();
+        }
+        catch(Exception e){
+            
+        }
+    }
      
-    
-    
 }
+
