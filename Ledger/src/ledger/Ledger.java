7d0e830e69f99ad84008ca791c57a8962e7c0b54
Aaditya Sharma <aadityaja@gmail.com>
2016-07-06 12:20:21 +0530

    Add files via upload
    Fixed INSERT query by using PreparedStatement instaed of Statement.

--- Ledger/src/ledger/Ledger.java
+++ Ledger/src/ledger/Ledger.java
@@ -7,7 +7,7 @@ import java.sql.*;
 
 public class Ledger  {
    public Connection c = null;
-   public Statement stmt = null;
+   public PreparedStatement statement = null;
    
    private String name_db = "";
    private String tm_db = "";
@@ -28,19 +28,24 @@ public class Ledger  {
     }
     
     // Query
-    public void insertToDb(String n, String t, String i, int q, int p){
-        name_db = n;
-        tm_db = t;
-        item_db = i;
-        quantity_db = q;
-        price_db = p;
+    public void insertToDb(String name_entry, String tmark_entry, String item_entry, int quantity_entry, int price_entry){
+        name_db = name_entry;
+        tm_db = tmark_entry;
+        item_db = item_entry;
+        quantity_db = quantity_entry;
+        price_db = price_entry;
         
         try{
-            connectDb();
-            stmt = c.createStatement();
-            String query = "INSERT INTO ledger_table (Name, Trade Mark, Item, Quantity, Price) VALUES (name_db, tm_db, item_db, quantity_db, price_db)";
-            stmt.executeUpdate(query);            
-            stmt.close();
+            connectDb();            
+            String query = "INSERT INTO ledger_table (Name, Trade_Mark, Item, Quantity, Price) VALUES (?, ?, ?, ?, ?)";
+            statement = c.prepareStatement(query);
+            statement.setString(1, name_db);
+            statement.setString(2, tm_db);
+            statement.setString(3, item_db);
+            statement.setInt(4, quantity_db);
+            statement.setInt(5, price_db);
+            statement.executeUpdate();            
+            
             c.commit();
             c.close();           
             
@@ -49,14 +54,16 @@ public class Ledger  {
         }             
     }
     
-    public void searchName(String n){
-        name_db = n;
+    public void searchName(String name_entry){
+        name_db = name_entry;
         try{
             connectDb();
-            stmt = c.createStatement();
-            String query = "SELECT * FROM ledger_table WHERE Name = name_db";
-            stmt.executeUpdate(query);
-            stmt.close();
+            
+            String query = "SELECT * FROM ledger_table WHERE Name = ?";
+            statement = c.prepareStatement(query);
+            statement.setString(1, name_db);
+            statement.executeQuery();
+            
             c.commit();
             c.close();
         }
@@ -65,14 +72,16 @@ public class Ledger  {
         }
     }
     
-    public void searchTM(String t){
-        tm_db = t;
+    public void searchTM(String tmark_entry){
+        tm_db = tmark_entry;
         try{
             connectDb();
-            stmt = c.createStatement();
-            String query = "SELECT * FROM ledger_table WHERE Trade Mark = tm_db";
-            stmt.executeUpdate(query);
-            stmt.close();
+            
+            String query = "SELECT * FROM ledger_table WHERE [Trade Mark] = ?";
+            statement = c.prepareStatement(query);
+            statement.setString(1, tm_db);
+            statement.executeQuery();
+            
             c.commit();
             c.close();
         }
@@ -81,14 +90,16 @@ public class Ledger  {
         }
     }
     
-    public void searchItem(String i){
-        item_db = i;
+    public void searchItem(String item_entry){
+        item_db = item_entry;
         try{
             connectDb();
-            stmt = c.createStatement();
-            String query = "SELECT * FROM ledger_table WHERE Item = item_db";
-            stmt.executeUpdate(query);
-            stmt.close();
+            
+            String query = "SELECT * FROM ledger_table WHERE Item = ?";
+            statement = c.prepareStatement(query);
+            statement.setString(1, item_db);
+            statement.executeQuery();
+            
             c.commit();
             c.close();
         }
